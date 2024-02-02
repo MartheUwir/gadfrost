dd73053cb4c00cd54c9d3dbff28d70c603485296
MarthUwir <uwrmaritha250@gmail.com>
2024-02-02 13:26:34 +0200

    yi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,32 @@
-
+import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Product from './Pages/Product';
 import ShopCategory from './Pages/ShopCategory';
-import {BrowserRouter, Routes,Route} from 'react-router-dom'
-import Cart from './Pages/Cart'
-import LoginSignup from './Pages/LoginSignup'
-import Shop from './Pages/Shop'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Shop from './Pages/Shop';
+import Hero  from './Components/Hero/Hero';
 
 function App() {
   return (
-    <div >
+    <div>
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-       <Route path='/' element ={<Shop/>}/>
-       <Route path='/mens' element ={<ShopCategory category="mens"/>}/>
-       <Route path='/womens' element ={<ShopCategory category="womens"/>}/>
-       <Route path='/kids' element ={<ShopCategory category="kids"/>}/>
-       <Route path="product" element={<Product/>}>
-       <Route path=':productId'element ={<Product/>}/>      
-       </Route>
-     <Route path='/cart' element={<Cart/>}/>
-     <Route path='/login' element={<LoginSignup/>}/>
-      </Routes>
+        <Navbar />
+        <Hero/>
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category="mens" />} />
+          <Route path='/womens' element={<ShopCategory category="womens" />} />
+          <Route path='/kids' element={<ShopCategory category="kids" />} />
+          <Route path="product" element={<Product />}>
+           <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
       </BrowserRouter>
-      <Product/>
- 
     </div>
   );
 }
